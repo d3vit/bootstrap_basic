d402af954bae68b4c350d8a08d2d10422e57ad71
Ryan Allen <ryan@ndigit.co>
2014-09-14 19:13:01 -0400

    Updated header code indentation

--- header.php
+++ header.php
@@ -43,16 +43,16 @@ else
 <body <? body_class($class);?>>
 	<div class="container">
         <div id="main-navigation" class="navbar navbar-default" role="navigation">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"><?php bloginfo( 'name' ); ?></a>
-          </div>
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          	<?php wp_nav_menu (array('menu'=>'Main Navigation', 'menu_class' =>'nav navbar-nav') ); ?>
-          </div><!-- /.navbar-collapse -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><?php bloginfo( 'name' ); ?></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            	<?php wp_nav_menu (array('menu'=>'Primary Menu', 'menu_class' =>'nav navbar-nav') ); ?>
+            </div><!-- /.navbar-collapse -->
         </div>
