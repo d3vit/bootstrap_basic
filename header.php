e55e9e9634a2859bb25e5005b9f9deb96a2925be
Ryan Allen <ryan@ndigit.co>
2014-09-16 15:37:18 -0400

    Update to pull the theme options into the header so they can be accessed on every page load.

--- header.php
+++ header.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+
+//Need to get the Theme Options
+$options = get_option('bootstrap_theme');
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!--[if IE]> 
 <html xmlns="http://www.w3.org/1999/xhtml" class="ie">
 <![endif]-->
